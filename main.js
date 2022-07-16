89fc92e1a50650732bf1457ae76651cee0a04b5d
adisonsud <adison.chire.broncano@gmail.com>
2022-07-16 00:58:32 -0500

    add variables js

---
+++ main.js
@@ -0,0 +1,9 @@
+const tasks = [];
+let time = 0
+let timer = null
+let timerBreak = null
+let current = null
+
+const bAdd = document.querySelector("#bAdd")
+const itTask = document.querySelector("#itTask")
+const form = document.querySelector("#form")
